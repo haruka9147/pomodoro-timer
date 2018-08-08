327d8cf29fe12bd6cea00076101605919d0cb2eb
jun29 <jun0814soccer@gmail.com>
2018-08-08 15:15:58 -0700

    added reset and long break

--- App.js
+++ App.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Dimensions
-} from 'react-native';
-import {
-  Constants
-} from 'expo';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Constants } from 'expo';
 
 const width = Dimensions.get('window').width; //full width
 
 class TimerButton extends React.Component {
   render() {
-    return ( <
-      TouchableOpacity style = {
-        this.props.buttonStyle
-      }
-      onPress = {
-        this.props.onPress
-      } >
-      <
-      Text style = {
-        styles.button
-      } > {
-        this.props.buttonText
-      } < /Text> <
-      /TouchableOpacity>
+    return (
+      <TouchableOpacity style={this.props.buttonStyle} onPress={this.props.onPress}>
+        <Text style={styles.button}>{this.props.buttonText}</Text>
+      </TouchableOpacity>
     )
   }
 }
@@ -39,14 +21,8 @@ class Counter extends React.Component {
     if (sec.toString().length == 1) {
       sec = '0' + sec.toString()
     }
-    return ( <
-      Text style = {
-        styles.counter
-      } > {
-        min
-      }: {
-        sec
-      } < /Text>
+    return (
+      <Text style={styles.counter}>{min}:{sec}</Text>
     )
   }
 }
@@ -56,27 +32,31 @@ class CountDownTimer extends React.Component {
     super(props);
     this.state = {
       count1: 1500,
-      count2: 600,
+      count2: 300,
+      count3: 600,
       counter1: null,
       currentCounter: 1,
-      currentCounter: 2
     }
   }
 
   start() {
     if (this.state.currentCounter === 1) {
       this.state.counter1 = setInterval(() => {
-        this.setState(prevState => ({
-          count1: prevState.count1 - 1
-        }))
+        if(this.state.count1 <= 1){
+          Vibration.vibrate([500, 500, 500]);
+          clearInterval(this.state.counter1)
+        }
+        this.setState(prevState => ({ count1: prevState.count1 - 1 }))
       }, 1000)
-    } else if (this.state.currentCounter === 2) {
+    }else if(this.state.currentCounter === 2){
       this.state.counter1 = setInterval(() => {
-        this.setState(prevState => ({
-          count2: prevState.count2 - 1
-        }))
+        this.setState(prevState => ({ count2: prevState.count2 - 1}))
       }, 1000)
-    } else {}
+    }else{
+      this.state.counter1 = setInterval(() => {
+        this.setState(prevState => ({ count3: prevState.count3 - 1 }))
+      }, 1000)
+    }
 
     // TODO: set long break timer
   }
@@ -84,7 +64,9 @@ class CountDownTimer extends React.Component {
   stop() {
     if (this.state.currentCounter === 1) {
       clearInterval(this.state.counter1)
-    } else if (this.state.currentCounter === 2) {
+    }else if(this.state.currentCounter === 2){
+      clearInterval(this.state.counter1)
+    }else {
       clearInterval(this.state.counter1)
     }
     // TODO: stop long break timer
@@ -92,6 +74,11 @@ class CountDownTimer extends React.Component {
 
   reset() {
     // reset timer
+    if (this.state.currentCounter === 1) {
+      this.setState({
+        count1: 10
+      })
+    }
   }
 
   setCurrentCounter(current) {
@@ -104,103 +91,36 @@ class CountDownTimer extends React.Component {
     let count;
     if (this.state.currentCounter === 1) {
       count = this.state.count1;
-    } else if (this.state.currentCounter === 2) {
+    }else if(this.state.currentCounter === 2){
       count = this.state.count2;
+    }else {
+      count = this.state.count3;
     }
     // TODO: set long break count
-    return ( <
-      View style = {
-        styles.container
-      } >
-      <
-      View style = {
-        styles.content
-      } >
-      <
-      View style = {
-        styles.buttons
-      } >
-      <
-      TimerButton buttonStyle = {
-        styles.buttonBg
-      }
-      buttonText = {
-        'Pomodoro'
-      }
-      onPress = {
-        this.setCurrentCounter.bind(this, 1)
-      }
-      /> <
-      TimerButton buttonStyle = {
-        [styles.buttonBg, styles.buttonMg]
-      }
-      buttonText = {
-        'Short Break'
-      }
-      onPress = {
-        this.setCurrentCounter.bind(this, 2)
-      }
-      /> <
-      TimerButton buttonStyle = {
-        styles.buttonBg
-      }
-      buttonText = {
-        'Long Break'
-      }
-      onPress = {
-        this.setCurrentCounter.bind(this, 3)
-      }
-      /> <
-      /View> <
-      Counter count = {
-        count
-      }
-      /> <
-      View style = {
-        styles.buttons
-      } >
-      <
-      TimerButton buttonStyle = {
-        styles.buttonBg
-      }
-      buttonText = {
-        'Start'
-      }
-      onPress = {
-        this.start.bind(this)
-      }
-      /> <
-      TimerButton buttonStyle = {
-        [styles.buttonBg, styles.buttonMg]
-      }
-      buttonText = {
-        'Stop'
-      }
-      onPress = {
-        this.stop.bind(this)
-      }
-      /> <
-      TimerButton buttonStyle = {
-        styles.buttonBg
-      }
-      buttonText = {
-        'Reset'
-      }
-      onPress = {
-        this.reset.bind(this)
-      }
-      /> <
-      /View> <
-      /View> <
-      /View>
+    return (
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <View style={styles.buttons}>
+            <TimerButton buttonStyle={styles.buttonBg} buttonText={'Pomodoro'} onPress={this.setCurrentCounter.bind(this, 1)} />
+            <TimerButton buttonStyle={[styles.buttonBg, styles.buttonMg]} buttonText={'Short Break'} onPress={this.setCurrentCounter.bind(this, 2)} />
+            <TimerButton buttonStyle={styles.buttonBg} buttonText={'Long Break'} onPress={this.setCurrentCounter.bind(this, 3)} />
+          </View>
+          <Counter count={count} />
+          <View style={styles.buttons}>
+            <TimerButton buttonStyle={styles.buttonBg} buttonText={'Start'} onPress={this.start.bind(this)} />
+            <TimerButton buttonStyle={[styles.buttonBg, styles.buttonMg]} buttonText={'Stop'} onPress={this.stop.bind(this)} />
+            <TimerButton buttonStyle={styles.buttonBg} buttonText={'Reset'} onPress={this.reset.bind(this)} />
+          </View>
+        </View>
+      </View>
     );
   }
 }
 
 export default class App extends React.Component {
   render() {
-    return ( <
-      CountDownTimer / >
+    return (
+      <CountDownTimer />
     )
   }
 }
